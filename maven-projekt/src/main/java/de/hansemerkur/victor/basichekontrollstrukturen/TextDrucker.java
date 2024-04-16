c2ede43835cccd7c23478d7aa7390213e6f708de
“hicapa23” <pyo.hica23@gmail.com>
2024-04-16 22:01:47 +0200

    actualizacion de todo el repositorio de vsc

---
+++ maven-projekt/src/main/java/de/hansemerkur/victor/basichekontrollstrukturen/TextDrucker.java
@@ -0,0 +1,72 @@
+package de.hansemerkur.victor.basichekontrollstrukturen;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class TextDrucker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int minRamdon = 1930;
+        int maxRamdon = 1950;
+
+        int numRamdonGenerator = random.nextInt(maxRamdon - minRamdon + 1) + minRamdon;
+
+        String redeMitOma = "";
+
+        int flascheMenge = 99;
+
+        while (flascheMenge > 0) {
+
+            System.out.println(flascheMenge + " bottles of beer on the wall, " + flascheMenge
+                    + " bottles of beer.Take one down and pass it around, " + (flascheMenge - 1)
+                    + " bottles of beer on the wall.\n");
+
+            flascheMenge--;
+
+        }
+
+        System.out.println(
+                "\no more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+
+        do {
+
+            System.out.println("\nWollen Sie etwas zu Oma sagen ja oder nein?");
+            var antwort = scanner.nextLine();
+
+            if ("ja".equalsIgnoreCase(antwort)) {
+
+                System.out.println("\nWas möchten Sie zu Oma sagen?");
+                var mitteilungFuerOma = scanner.nextLine();
+
+                System.out.println("\nMitteilung für Oma: " + mitteilungFuerOma + "\n");
+
+                if (mitteilungFuerOma.equals(mitteilungFuerOma.toUpperCase())) {
+
+                    System.out.println("\nNEIN, NICHT SEIT " + numRamdonGenerator + "\n");
+
+                } else {
+                    System.out.println("\nHUH?!  SPREICH, SONNY!\n");
+                }
+
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("\nMoechten Sie noch mit Oma weiter reden? Wenn nicht, schreiben Sie 'BYE' ");
+                    redeMitOma = scanner.nextLine();
+
+                    if (!"BYE".equals(redeMitOma)) {
+                        break;
+                    }
+                }
+
+            }
+
+            else {
+                break;
+            }
+
+        } while (!"BYE".equals(redeMitOma));
+        scanner.close();
+    }
+
+}
---
+++ maven-projekt/src/main/java/de/hansemerkur/victor/zahlenubersetzer/ZahlenUbersetzer.java
@@ -0,0 +1,46 @@
+package de.hansemerkur.victor.zahlenubersetzer;
+
+import java.util.Scanner;
+
+public class ZahlenUbersetzer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String romicheZahl;
+        int zahl;
+
+        System.out.println("Geben Sie eine ganze Zahl zwischen 1 und 3999:");
+        zahl = scanner.nextInt();
+
+        if (zahl > 1 && zahl < 3999) {
+            romicheZahl = konvertionZuRomischenZahlen(zahl);
+            System.out.println(zahl + " In romischen Zahlen ist: " + romicheZahl);
+        }
+
+        else {
+
+            System.out.println("Die nummer soll im Rang sein");
+        }
+        scanner.close();
+    }
+
+    public static String konvertionZuRomischenZahlen(int zahl) {
+
+        String[] einheiten = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        String[] zehner = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        String[] hunderter = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        String[] tausende = { "", "M", "MM", "MMM" };
+
+        int indexEinheiten, indexZehner, indexHunderter, indexTausende;
+
+        indexTausende = zahl / 1000;
+        zahl = zahl % 1000;
+        indexHunderter = zahl / 100;
+        zahl = zahl % 100;
+        indexZehner = zahl / 10;
+        zahl = zahl % 10;
+        indexEinheiten = zahl;
+
+        return tausende[indexTausende] + hunderter[indexHunderter] + zehner[indexZehner] + einheiten[indexEinheiten];
+
+    }
+}
